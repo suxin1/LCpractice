8457a7ff8d46dee140e4e9900c59e25083c2d9af
何书鑫 <heshuxin@1m2.net>
2020-07-02 17:59:05 +0800

    添加注释

--- C/maxArea.c
+++ C/maxArea.c
@@ -1,3 +1,10 @@
+/**
+ *  给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+ * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 说明：你不能倾斜容器，且 n 的值至少为 2。
+*/
+
 #define true 1
 #define false 0
 
@@ -18,7 +25,6 @@ int main(void) {
 
 
 int maxArea(int* height, int heightSize){
-    if(heightSize == 2) return height[0]*height[1];
     int i = 0, j = heightSize - 1;
     int area = (j-i) * (height[i]<height[j]?height[i]:height[j]);
     while(i<j) {
