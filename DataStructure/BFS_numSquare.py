8356c9d968a5d0b96f6a7b177dca5dc146715aa3
何书鑫 <heshuxin@1m2.net>
2020-07-10 18:04:30 +0800

    add BFS_numSquare.py

--- DataStructure/BFS_numSquare.py
+++ DataStructure/BFS_numSquare.py
@@ -1,7 +1,11 @@
+"""
+给定一个数N，使平方数(1, 4, 9, 16, ...)相加的和等于N，求最少要用多少个平方数。
+"""
+
 from collections import deque
 import math
 
-
+# 粗暴求解
 def numSquares(n: int) -> int:
     squareList = [(i + 1) ** 2 for i in range(int(math.sqrt(n)))]
     squareList.reverse()
@@ -20,4 +24,38 @@ def numSquares(n: int) -> int:
 
     return -1
 
-print(numSquares(7168))
+# num_squares(n) = min{num_squares((n - k)) + 1 ||　ｋ属于任意平方数}
+# 粗暴求解算法2
+def num_squares_v2(n: int) -> int:
+    square_list = [(i + 1) ** 2 for i in range(int(math.sqrt(n)))]
+
+    def minNumSquare(m):
+        if m in square_list:
+            return 1
+        min_num = float('inf')
+        for square in square_list:
+            if m < square:
+                break
+            new_num = minNumSquare(m - square) + 1
+            min_num = min(new_num, min_num)
+        return min_num
+    return minNumSquare(n)
+
+
+# num_squares(n) = min{num_squares((n - k)) + 1 ||　ｋ属于任意平方数}
+# 动态规划 (Dynamic programing)
+# 从1求解最优解直到N
+def num_squares_fast(n: int) -> int:
+    square_list = [(i + 1) ** 2 for i in range(int(math.sqrt(n)))]
+
+    dp = [float('inf')] * (n + 1)
+    dp[0] = 0
+    for i in range(1, n+1):
+        for s in square_list:
+            if i < s:
+                break
+            dp[i] = min(dp[i], dp[i-s] + 1)
+    return dp[-1]
+
+
+print(num_squares_fast(7168))
