ad2d82066f381ffd2eab5186a64c4e7c2d4a3c09
heshuxin <suxin1@live.com>
2020-07-31 00:51:15 +0800

    udpate

--- DataStructure/STACK_string.c
+++ DataStructure/STACK_string.c
@@ -10,28 +10,40 @@ typedef int8_t bool;
 
 
 bool isValid(char *s);
-
+void function(char *s);
 int main(void) {
     printf("start\n");
-    bool res = isValid("(((())))");
-    printf("%d", res);
+    char *s = "(((())))";
+    function(s);
+    bool res = isValid(s);
+    printf("res:%d", res);
 }
 
+void function(char *s) {
+    int a = (int)strlen(s);
+    printf("length: %d\n", a);
+}
+
+
 bool isValid(char *s) {
+    printf(">>>>>------------>\n");
     Stack stack;
     stack_init(strlen(s), stack);
-
+    printf(">>>>>------------>\n");
     for (int i=0;i<strlen(s);i++) {
+        printf("----%d\n", i);
         if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
             stack_add(stack, s[i]);
             continue;
         }
         if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
             int prev = stack_get(stack);
+            printf("stack out value: %d\n", prev);
             if(prev < 0) {
                 return FALSE;
             }
             char temp_c = (char)prev;
+            printf("stack out char: %c\n", temp_c);
             if (s[i] == ')' && temp_c == '(') {
                 continue;
             }
