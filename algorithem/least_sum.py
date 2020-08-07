1c63ceafee0c544ade48b253768a3af7fad78185
heshuxin <suxin1@live.com>
2020-08-08 00:35:03 +0800

    update

--- algorithem/least_sum.py
+++ algorithem/least_sum.py
@@ -2,6 +2,7 @@
 给一个数组l和一个整型数字n，找到一个l的子数组使其和大于等于n且长度最小。
 """
 
+
 def least_sum(l, n):
     start = 0
     length = float("inf")
@@ -16,7 +17,8 @@ def least_sum(l, n):
                 sub_array = l[start:i+1]
             sum -= l[start]
             start += 1
-    return (length, sub_array)
+
+    return length, sub_array
 
 
 a = [1, 3, 5, 3, 2, 6, 5, 4, 1, 3, 4]
