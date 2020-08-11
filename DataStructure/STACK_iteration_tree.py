d49aa9bb3b6b585ca6c72db3846365fbca21f37c
heshuxin <suxin1@live.com>
2020-08-12 00:18:35 +0800

    add STACK_iteration_tree.py

---
+++ DataStructure/STACK_iteration_tree.py
@@ -0,0 +1,52 @@
+from typing import List
+
+
+class Node(object):
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+
+# 前序递归（中，左，右）
+def preorder_iteration(root):
+    result = list()
+    stack = [root]
+    while stack:
+        cur = stack.pop()
+        if cur:
+            result.append(cur.val)
+            stack.append(cur.right)
+            stack.append(cur.left)
+
+    return result
+
+
+def tree_factory(tree: List):
+    """
+    a tree is like [[1, 2, 3], [2, 4, 5]
+    :param tree:
+    :return:
+    """
+    memo = dict()
+
+    for node in tree:
+        memo[node[0]] = Node(node[0])
+
+    for node in tree:
+        try:
+            memo[node[0]].left = memo[node[1]]
+        except:
+            pass
+        try:
+            memo[node[0]].right = memo[node[2]]
+        except:
+            pass
+
+    return memo[tree[0][0]]
+
+
+list_tree = [[1, 2, 3], [2, 4, 5], [3, 6, 7], [4], [5], [6], [7]]
+root = tree_factory(list_tree)
+
+print(preorder_iteration(root))
